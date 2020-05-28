1a84d89488ab7c06ecfbe3bcc35337110128e307
m-sallam <muhammad.sallam@outlook.com>
2020-05-29 00:34:32 +0200

    v3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}! v2`);
+  res.send(`Hello ${target}! v3`);
 });
 
 const port = process.env.PORT || 8080;
